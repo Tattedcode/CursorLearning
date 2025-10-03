fc0d9e32be0c70ea5ef2068acf7883c38a9ea59e
Liam Brown <tattedexplorer@gmail.com>
2025-10-03 23:05:52 +0800

    Initial Commit

---
+++ PinkyCursorTeach/PinkyCursorTeachApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinkyCursorTeachApp.swift
+//  PinkyCursorTeach
+//
+//  Created by Liam Brown on 3/10/2568 BE.
+//
+
+import SwiftUI
+
+@main
+struct PinkyCursorTeachApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
